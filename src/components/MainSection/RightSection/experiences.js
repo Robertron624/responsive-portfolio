1b6dda4b2b3dd719004fe159b3c30eb1caa49efc
Roberto Ramirez <robert.624@hotmail.com>
2023-11-21 12:41:35 -0500

    feat: Experiences section

---
+++ src/components/MainSection/RightSection/experiences.js
@@ -0,0 +1,16 @@
+export const experiences = [
+    {
+        position: 'Frontend Developer Trainee',
+        company: 'IT Globers',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        date: '2022 - Present',
+        companyLogo: '/images/experiences/itglobers.svg',
+    },
+    {
+        position: 'Frontend Developer',
+        company: 'Blacksip',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        date: '2021 - 2022',
+        companyLogo: '/images/experiences/blacksip.webp',
+    }
+]
